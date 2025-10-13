ed0848b2cd062353389a4610ec24b161e5893748
Weronika Woroszczuk <weronika.woroszczuk@bluesoft.com>
2025-10-13 02:39:23 +0200

    fix: Clean up formatting in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	integrations: [tailwind()],
-    site: 'https://wworoszczuk-blue.github.io',
-    base: '/ELIVION-landing-page',
-    outDir: './dist'
+  integrations: [tailwind()],
+  site: 'https://wworoszczuk-blue.github.io',
+  outDir: './dist'
 });
