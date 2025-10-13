c0f86dc6b61fa66b041d76c56592e6f0f5b24842
Weronika Woroszczuk <weronika.woroszczuk@bluesoft.com>
2025-10-13 02:24:54 +0200

    feat: Add GitHub Actions workflow for deployment and configure Astro settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
 	integrations: [tailwind()],
+    site: 'https://wworoszczuk-blue.github.io',
+    base: '/ELIVION-landing-page',
+    outDir: './dist'
 });
