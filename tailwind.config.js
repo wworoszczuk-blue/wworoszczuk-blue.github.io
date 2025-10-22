cccb5e7c2c55a0a86b2b7ad0839f683d5c609d53
Weronika Woroszczuk <weronika.woroszczuk@bluesoft.com>
2025-10-22 23:55:50 +0200

    Fix fade in

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,21 +15,6 @@ export default {
         main: ['"Montserrat"', 'sans-serif'],
         logo: ['"Mina"', 'sans-serif'],
       },
-      animation: {
-        'scroll-fade-in': 'scroll-fade-in 0.8s ease-out forwards',
-      },
-      keyframes: {
-        'scroll-fade-in': {
-          from: {
-            opacity: '0',
-            transform: 'translateY(30px)',
-          },
-          to: {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-      },
     }
   },
   plugins: [],
